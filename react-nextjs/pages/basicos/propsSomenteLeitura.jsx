45fd03b10f9945bb616c60cb650bcecea7c975c3
regisrocha <regis.rocha@mercadolivre.com>
2022-10-30 21:17:58 -0300

    Component com prop somente leitura

---
+++ react-nextjs/pages/basicos/propsSomenteLeitura.jsx
@@ -0,0 +1,9 @@
+import SomaUm from "../components/SomaUm";
+
+export default function propsSomenteLeitura(props) {
+    return (
+            <div>
+                <SomaUm numero={1}/>
+            </div>
+    )
+}
